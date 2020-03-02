51eac8ad3bed9b45b6596c2673cc431c4397cda6
Svein Gunnar Larsen <svein.gunnar.larsen@gmail.com>
2020-03-02 20:55:25 +0100

    removed image-type

--- src/git/utils.ts
+++ src/git/utils.ts
@@ -1,13 +1,9 @@
-
-import * as imageType from "image-type";
-import {arrayBufferToBase64} from "../helpers/utils";
-
+import {arrayBufferToBase64, isImage} from "../helpers/utils";
 const decoder = new TextDecoder('utf8');
 
 export async function getFileContent(pfs, filePath: string) {
     const buffer = await pfs.readFile(filePath);
-    const imageMeta = imageType(buffer);
-    if (imageMeta) {
+    if (isImage(filePath)) {
         return arrayBufferToBase64(buffer);
     } else {
         return decoder.decode(buffer);
