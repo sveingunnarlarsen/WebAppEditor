c15341d40f2613afbaf3ed09e5064e3f37aab047
Svein Gunnar Larsen <sveingunnarlarsen@gmail.com>
2020-05-18 17:01:41 +0200

    clean up

--- src/completer/index.ts
+++ src/completer/index.ts
@@ -1,43 +1,43 @@
 import store from "../store";
 import MonacoManager from "../monaco";
-import {LanguageClient as LanguageClientType, ClientEvent} from "../types/language-client";
-import {provideDiagnostics} from "./providers/diagnosticProvider";
-import {CompletionItemProvider} from "./providers/completionItemProvider";
-import {SignatureHelpProvider} from "./providers/signatureHelpProvider";
-import {HoverProvider} from "./providers/hoverProvider";
-import {DefinitionProvider} from "./providers/definitionProvider";
-import {ReferenceProvider} from "./providers/referenceProvider";
+import { LanguageClient as LanguageClientType, ClientEvent } from "../types/language-client";
+import { provideDiagnostics } from "./providers/diagnosticProvider";
+import { CompletionItemProvider } from "./providers/completionItemProvider";
+import { SignatureHelpProvider } from "./providers/signatureHelpProvider";
+import { HoverProvider } from "./providers/hoverProvider";
+import { DefinitionProvider } from "./providers/definitionProvider";
+import { ReferenceProvider } from "./providers/referenceProvider";
 
 let appId;
 //@ts-ignore
 const client: LanguageClientType = new LanguageClient();
 
-(async function() {
-	try {
-	    const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+(async function () {
+    try {
+        const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
         const lsUrl = `${wsProtocol}://${window.location.hostname}:8082`;
-		await client.connect(lsUrl);
-		console.log("Language client connected");
-        
+        await client.connect(lsUrl);
+        console.log("Language client connected");
+
         // @ts-ignore
         client.on("disconnected", async (closeEvent) => {
-            if(closeEvent.wasClean) return;
+            if (closeEvent.wasClean) return;
             await new Promise(r => setTimeout(r, 1000));
             const result = await client.connect(lsUrl);
-            if(result.success) {
-                if(!appId) return;
+            if (result.success) {
+                if (!appId) return;
                 client.initialize(appId);
                 console.log('LanguageClient reconnected');
             }
         });
 
-	} catch (e) {
-		console.log("Error connecting to language server", e);
-	}
+    } catch (e) {
+        console.log("Error connecting to language server", e);
+    }
 
-	if (client.isConnected) {
-		store.subscribe(handleChange);
-	}
+    if (client.isConnected) {
+        store.subscribe(handleChange);
+    }
 
     const monacoInstance = await MonacoManager.getInstance();
     window.monaco = monacoInstance;
@@ -45,9 +45,9 @@ const client: LanguageClientType = new LanguageClient();
     monacoInstance.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
         noSemanticValidation: true,
         noSyntaxValidation: true,
-        noSuggestionDiagnostics: true,        
+        noSuggestionDiagnostics: true,
     });
-    
+
     monacoInstance.languages.registerCompletionItemProvider('typescript', new CompletionItemProvider(client));
     monacoInstance.languages.registerSignatureHelpProvider('typescript', new SignatureHelpProvider(client));
     monacoInstance.languages.registerHoverProvider('typescript', new HoverProvider(client));
@@ -55,20 +55,20 @@ const client: LanguageClientType = new LanguageClient();
     monacoInstance.languages.registerReferenceProvider('typescript', new ReferenceProvider(client));
 
 
-	client.on('publishDiagnostics', (result) => {
-		provideDiagnostics(result);
-	});
+    client.on('publishDiagnostics', (result) => {
+        provideDiagnostics(result);
+    });
 })();
 
 function handleChange() {
-	if (store.getState().app.id && store.getState().app.id !== appId) {
-		appId = store.getState().app.id;
-		client.initialize(appId);
-		console.log("Language client initialized for project", appId);
-	}
+    if (store.getState().app.id && store.getState().app.id !== appId) {
+        appId = store.getState().app.id;
+        client.initialize(appId);
+        console.log("Language client initialized for project", appId);
+    }
 }
 
-export async function fileUpdated({path, content} : {path: string, content: string}) {
+export async function fileUpdated({ path, content }: { path: string, content: string }) {
     if (client.isReady) {
         try {
             await client.textDocumentChanged(path, content);
@@ -81,7 +81,7 @@ export async function fileUpdated({path, content} : {path: string, content: stri
 export async function fileDeleted(path: string) {
     if (client.isReady) {
         try {
-            await client.textDocumentDeleted(path);   
+            await client.textDocumentDeleted(path);
             MonacoManager.deleteModel(path);
         } catch (e) {
             console.log("Language client error", e);
@@ -93,7 +93,7 @@ export async function fileCreated(path: string, type: 'file' | 'foler' = 'file',
     if (client.isReady) {
         try {
             await client.textDocumentCreated(path, type, content);
-            MonacoManager.createModel({path, type, content});
+            MonacoManager.createModel({ path, type, content });
         } catch (e) {
             console.log("Language client error", e);
         }
@@ -102,8 +102,8 @@ export async function fileCreated(path: string, type: 'file' | 'foler' = 'file',
 
 export async function fileOpened(path: string) {
     if (client.isReady) {
-        try {            
-			await client.textDocumentOpened(path);
+        try {
+            await client.textDocumentOpened(path);
         } catch (e) {
             console.log("Language client error: ", e);
         }
